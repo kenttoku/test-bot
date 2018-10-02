2a04f0c4d5f280b7cd7678fc1db7a9638c8f6d57
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-02 14:58:18 -0400

    Setup basic bot

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+const Discord = require('discord.js');
+const { TOKEN } = require('./config');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+  if (message.content === 'ping') {
+    message.channel.send('pong');
+  }
+});
+
+bot.login(TOKEN);
